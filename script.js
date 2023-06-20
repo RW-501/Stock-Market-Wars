08e973d310e1e6d61a248d155982b1db14ed5bc0
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-06-20 04:10:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -277,17 +277,19 @@ for (const [name, { stockQuantity, totalCost }] of Object.entries(portfolio)) {
   // Create a new table row for the company in the portfolio display
   const row = document.createElement("tr");
 
+  
   // Create table cells for the company name, stock price, average stock price, stock quantity, and company value
   const nameCell = document.createElement("td");
   nameCell.textContent = name;
   const priceCell = document.createElement("td");
-  priceCell.textContent = "$" + stockPrice.toFixed(2);
+  priceCell.textContent = stockPrice ? "$" + stockPrice.toFixed(2) : ""; // Check if stockPrice is defined
   const totalCostCell = document.createElement("td");
-  totalCostCell.textContent = "$" + (totalCost || ""); // Display the average stock price
+  totalCostCell.textContent = totalCost ? "$" + totalCost.toFixed(2) : ""; // Check if totalCost is defined
   const quantityCell = document.createElement("td");
-  quantityCell.textContent = stockQuantity || "";
+  quantityCell.textContent = stockQuantity !== undefined ? stockQuantity : ""; // Check if stockQuantity is defined
   const valueCell = document.createElement("td");
-  valueCell.textContent = "$" + (companyValue || "");
+  valueCell.textContent = companyValue ? "$" + companyValue.toFixed(2) : ""; // Check if companyValue is defined
+
 
   // Append the table cells to the row
   row.appendChild(nameCell);
