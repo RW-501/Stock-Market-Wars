b164fa88a4d068fec160b71d9d43597c56fe1416
Shotta_501 <92776143+RW-501@users.noreply.github.com>
2023-06-24 15:50:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -709,7 +709,7 @@ if (counterValue > 365) {
   
   
   // Increment the new week counter
-  if (incrementCounter >= 7) {
+  if (incrementCounter > 7) {
     incrementCounter = 0;
     newWeek(); // Call the newWeek function
   } else
